7a14b1a58e12e75a1b698e3ce6278e1fdcbfa3a5
Sheharyar Javed <sheharyarj@gmail.com>
2024-03-25 22:19:22 +0500

    made navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,30 @@
-import  { useState } from "react";
+import  { useState ,useRef} from "react";
 import styles from './Navbar.module.css';
 import logo from '../assets/logo.png';
 import cart_icon from '../assets/cart_icon.png';
 import {Link} from 'react-router-dom';
 import { useContext } from "react";
 import { ShopContext } from "../../context/ShopContext";
+import hamburger from '../assets/hamburger.png'
 
 function Navbar()
 {
     const [menu,setMenu] = useState("shop");
     const {getTotalCartItems}= useContext(ShopContext);
+    const menuRef = useRef();
 
-    console.log("menue : " ,menu);
+    const dropdown_toggle =(event) =>{
+        menuRef.current.classList.toggle(styles.nav_menu_visible);
+        event.target.classList.toggle(styles.open);
+    }
     return(
         <div className={styles.navbar}>
-            <div className={styles.nav_log0}>
+            <div className={styles.nav_logo}>
                 <img src={logo} alt="" />
                 <p>SHOPPER</p>
-
             </div>
-            <ul className={styles.nav_menu}>
+            <img className={styles.navbar_dropdown} onClick={(event)=>dropdown_toggle(event)} src={hamburger} alt="" />
+            <ul ref={menuRef} className={styles.nav_menu}>
                 <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration :'none'}} to="/">Shop</Link> {menu==="shop"?<hr/>:<></>}</li>
                 <li onClick={()=>{setMenu("men")}}><Link style={{textDecoration :'none'}} to="/men">Men</Link> {menu==="men"?<hr/>:<></>}</li>
                 <li onClick={()=>{setMenu("women")}}><Link style={{textDecoration :'none'}} to="/women">Women</Link> {menu==="women"?<hr/>:<></>}</li>
@@ -32,8 +37,6 @@ function Navbar()
                 {getTotalCartItems()}
             </div>
             </div>
-          
-
         </div>
     )
 }
